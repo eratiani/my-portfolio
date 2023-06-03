4c942d6f909596530b5f54b05d5009130a0f9a0b
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-06-03 15:21:22 +0400

    add projects data, minor structure change

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -6,8 +6,10 @@ import { CoreModule } from '../core/core.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectComponent } from './components/project/project.component';
+import { OtherComponent } from './components/other/other.component';
 @NgModule({
-  declarations: [MainViewComponent, ProjectsComponent, SkillsComponent],
+  declarations: [MainViewComponent, ProjectsComponent, SkillsComponent, ProjectComponent, OtherComponent],
   imports: [CommonModule, CoreModule, MatSidenavModule],
 })
 export class MainPageModule {}
